cdc61a5051066ca238741037eb2e27c27c34e71d
Skrylar <joshua.cearley@gmail.com>
2019-02-03 05:58:11 -0600

    readme updates, and made text fields prettier

--- src/jack-audio-module-widget.cc
+++ src/jack-audio-module-widget.cc
@@ -3,16 +3,62 @@
 #include "hashids.hh"
 #include "components.hh"
 
-struct JackPortLedTextField : LedDisplayTextField {
-	int managed_port;
-	JackAudioModuleWidget* master;
+namespace rack {
+   extern std::shared_ptr<Font> gGuiFont;
+}
 
-	JackPortLedTextField() : LedDisplayTextField() {}
+struct JackPortLedTextField : public LedDisplayTextField {
+   int managed_port;
+   JackAudioModuleWidget* master;
 
-	virtual void onTextChange() {
-		LedDisplayTextField::onTextChange();
-		master->on_port_renamed(managed_port, text);
+   JackPortLedTextField() : LedDisplayTextField() {
+      font = Font::load(assetPlugin(plugin, "res/3270Medium.ttf"));
+   }
+
+   void draw(NVGcontext *vg) override {
+	nvgScissor(vg, 0, 0, box.size.x, box.size.y);
+
+	NVGcolor fill = nvgRGB(20, 39, 53);
+	NVGcolor color = nvgRGB(255, 255, 255);
+
+	// Background
+	nvgBeginPath(vg);
+	nvgRoundedRect(vg, 0, 0, box.size.x, box.size.y, 5.0);
+	nvgFillColor(vg, fill);
+	nvgFill(vg);
+
+	// Text
+	if (font->handle >= 0) {
+	   bndSetFont(font->handle);
+
+
+
+	   NVGcolor highlightColor = color;
+	   highlightColor.a = 0.5;
+
+	   int begin = min(cursor, selection);
+	   int end = (this == gFocusedWidget) ? max(cursor, selection) : -1;
+	   bndIconLabelCaret(vg,
+			     textOffset.x, textOffset.y,
+			     box.size.x - 2*textOffset.x,
+			     box.size.y - 2*textOffset.y,
+			     -1,
+			     color,
+			     13, // font size
+			     text.c_str(),
+			     highlightColor,
+			     begin, end);
+
+	   bndSetFont(gGuiFont->handle);
 	}
+
+	nvgResetScissor(vg);
+   }
+
+   void onTextChange() override {
+      LedDisplayTextField::onTextChange();
+      master->on_port_renamed(managed_port, text);
+   }
 };
 
 JackAudioModuleWidget::JackAudioModuleWidget(JackAudioModule *module) : ModuleWidget(module) {
@@ -108,7 +154,7 @@ JackAudioModuleWidget::JackAudioModuleWidget(JackAudioModule *module) : ModuleWi
 	char port_name[128];
 	hashidsxx::Hashids hash("grilled cheese sandwiches");
 	std::string id = hash.encode(reinterpret_cast<size_t>(module));
-	
+
 	for (int i = 0; i < JACK_PORTS; i++) {
 	  snprintf(reinterpret_cast<char*>(&port_name), 128, "%s:%d", id.c_str(), i);
 	  // XXX using setText here would cause crashes because it would try to tell

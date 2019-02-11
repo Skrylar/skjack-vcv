4e4af24729508ec4be7988f3a7323eb7cf713712
Skrylar <joshua.cearley@gmail.com>
2019-02-10 22:00:55 -0600

    adapt jack dsp code to check role of modules

--- src/skjack.cc
+++ src/skjack.cc
@@ -22,27 +22,58 @@ int on_jack_process(jack_nframes_t nframes, void *) {
    {
       auto module = *itr;
 
-      auto available = module->jack_output_buffer.size();
-      if (available >= nframes) {
-	 jack_default_audio_sample_t* jack_buffer[JACK_PORTS];
-	 for (int i = 0; i < JACK_PORTS; i++) {
-	    jack_buffer[i] = module->jport[i].get_audio_buffer(nframes);
-	 }
+      switch (module->role) {
+	 case jack_audio_module_base::ROLE_DUPLEX: {
+	    auto available = module->jack_output_buffer.size();
+	    if (available >= nframes) {
+	       jack_default_audio_sample_t* jack_buffer[JACK_PORTS];
+	       for (int i = 0; i < JACK_PORTS; i++) {
+		  jack_buffer[i] = module->jport[i].get_audio_buffer(nframes);
+	       }
 
-	 for (jack_nframes_t i = 0; i < nframes; i++) {
-	    Frame<AUDIO_OUTPUTS> output_frame = module->jack_output_buffer.shift();
-	    for (int j = 0; j < AUDIO_OUTPUTS; j++) {
-	       jack_buffer[j][i] = output_frame.samples[j];
+	       for (jack_nframes_t i = 0; i < nframes; i++) {
+		  Frame<AUDIO_OUTPUTS> output_frame = module->jack_output_buffer.shift();
+		  for (int j = 0; j < AUDIO_OUTPUTS; j++) {
+		     jack_buffer[j][i] = output_frame.samples[j];
+		  }
+
+		  Frame<AUDIO_INPUTS> input_frame;
+		  for (int j = 0; j < AUDIO_INPUTS; j++) {
+		     input_frame.samples[j] = jack_buffer[j+AUDIO_OUTPUTS][i];
+		  }
+		  module->jack_input_buffer.push(input_frame);
+	       }
+
+	       module->output_latch.reset();
 	    }
+	 } break;
 
-	    Frame<AUDIO_INPUTS> input_frame;
-	    for (int j = 0; j < AUDIO_INPUTS; j++) {
-	       input_frame.samples[j] = jack_buffer[j+AUDIO_OUTPUTS][i];
+	 case jack_audio_module_base::ROLE_OUTPUT: {
+	    auto available = module->jack_output_buffer.size();
+	    if (available >= nframes) {
+	       jack_default_audio_sample_t* jack_buffer[JACK_PORTS];
+	       for (int i = 0; i < JACK_PORTS; i++) {
+		  jack_buffer[i] = module->jport[i].get_audio_buffer(nframes);
+	       }
+
+	       for (jack_nframes_t i = 0; i < nframes; i++) {
+		  Frame<AUDIO_OUTPUTS> output_frame =
+		     module->jack_output_buffer.shift();
+		  Frame<AUDIO_INPUTS> input_frame =
+		     module->jack_input_buffer.shift();
+		  
+		  for (int j = 0; j < AUDIO_OUTPUTS; j++) {
+		     jack_buffer[j][i] = output_frame.samples[j];
+		  }
+
+		  for (int j = 0; j < AUDIO_INPUTS; j++) {
+		     jack_buffer[j+AUDIO_OUTPUTS][i] = input_frame.samples[j];
+		  }
+	       }
+
+	       module->output_latch.reset();
 	    }
-	    module->jack_input_buffer.push(input_frame);
-	 }
-
-	 module->output_latch.reset();
+	 } break;
       }
    }
 

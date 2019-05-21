4129a6eff6856e32e5c3af3dbfc7797fb37d6584
Joshua Cearley <joshua.cearley@gmail.com>
2019-05-21 02:15:38 -0500

    perl -i -pe 's/Model::create/createModel/g' src/*

--- src/jack-audio-module-widget.cc
+++ src/jack-audio-module-widget.cc
@@ -295,10 +295,10 @@ void jack_audio_module_widget_base::on_port_renamed(int port, const std::string&
 // change), human-readable module name, and any number of tags
 // (found in `include/tags.hpp`) separated by commas.
 Model* jack_audio_model =
-   Model::create<JackAudioModule, JackAudioModuleWidget>("JackAudio");
+   createModel<JackAudioModule, JackAudioModuleWidget>("JackAudio");
 
 Model* jack_audio_out8_model =
-   Model::create<jack_audio_out8_module, jack_audio_out8_module_widget>("JackAudioOut8");
+   createModel<jack_audio_out8_module, jack_audio_out8_module_widget>("JackAudioOut8");
 
 Model* jack_audio_in8_model =
-   Model::create<jack_audio_in8_module, jack_audio_in8_module_widget>("JackAudioIn8");
+   createModel<jack_audio_in8_module, jack_audio_in8_module_widget>("JackAudioIn8");
